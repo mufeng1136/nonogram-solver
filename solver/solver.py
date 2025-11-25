fed278d80aec800b4173f671fb0ba7168f8514fe
worldedge <jiashenzhang1933@outlook.com>
2025-11-25 17:05:47 +0100

    add more validation rule modify the structure of project

--- solver/solver.py
+++ solver/solver.py
@@ -1,10 +1,44 @@
 from itertools import chain
-from utils.math_utils import integer_partitions
 from itertools import groupby
 import logging
 
 # Module logger
 logger = logging.getLogger(__name__)
+def integer_partitions(
+    n: int, k: int, current: list[int] | None = None
+) -> list[list[int]]:
+    """Generate all ordered compositions of integer n into k parts.
+
+    Args:
+        n: The integer to partition
+        k: The number of parts to partition into
+        current: Current partition being built (used internally for recursion)
+
+    Returns:
+        A list of all possible compositions, where each composition is a list of k positive integers
+        that sum to n. Different orderings are treated as different compositions.
+
+    Example:
+        >>> integer_partitions(5, 2)
+        [[1, 4], [2, 3], [3, 2], [4, 1]]
+        >>> integer_partitions(4, 3)
+        [[1, 1, 2], [1, 2, 1], [2, 1, 1]]
+    """
+    if current is None:
+        current = []
+
+    if k == 1:
+        return [current + [n]]
+
+    if k > n:
+        return []
+
+    result = []
+
+    for i in range(1, n - k + 2):
+        result.extend(integer_partitions(n - i, k - 1, current + [i]))
+
+    return result
 
 class NonogramSolver:
     def __init__(
@@ -86,6 +120,11 @@ class NonogramSolver:
             if min_cells_needed > self.row:
                 self.ready_to_solve = False
                 return False
+            
+        col_clue_sums = sum(sum(clue) for clue in self.col_clues)
+        row_clue_sums = sum(sum(clue) for clue in self.row_clues)
+        if col_clue_sums != row_clue_sums:
+            return False
 
         self.ready_to_solve = True
         return True
--- utils/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Utility modules for nonogram solver."""
-
-# Use relative import to bring utility functions into package namespace
-from .math_utils import *
-
-# Export all non-private names imported into this package
-__all__ = [name for name in globals() if not name.startswith("_")]
